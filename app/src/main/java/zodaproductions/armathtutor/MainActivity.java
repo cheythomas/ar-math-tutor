45b9533308d92970914bbe3aa8a3a97239cf31f3
cheythomas <tcheyenne360@yahoo.com>
2018-11-05 22:51:01 -0800

    testing camera

--- app/src/main/java/zodaproductions/armathtutor/MainActivity.java
+++ app/src/main/java/zodaproductions/armathtutor/MainActivity.java
@@ -1,8 +1,23 @@
 package zodaproductions.armathtutor;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,13 +26,21 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import java.util.Arrays;
+
+
+import android.support.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,10 +59,24 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    TextureView textureView;
+    CameraDevice cameraDevice;
+    String cameraId;
+    Size imageDimensions;
+
+    Handler backgroundHandler;
+    HandlerThread handlerThread;
+
+    CaptureRequest.Builder captureRequest;
+    CameraCaptureSession cameraSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textureView = (TextureView) findViewById(R.id.texture);
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -56,17 +93,178 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
+
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int wid, int len) {
+            try {
+                openCamera();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int wid, int len) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+
+    private void openCamera() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        cameraId = cameraManager.getCameraIdList()[0];
+
+        CameraCharacteristics cc = cameraManager.getCameraCharacteristics(cameraId);
+        StreamConfigurationMap map = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+        imageDimensions = map.getOutputSizes(SurfaceTexture.class)[0];
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        cameraManager.openCamera(cameraId, stateCallback, null);
+
+    }
+
+    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            try {
+                startCameraPreview();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int err) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    } ;
+
+    private void startCameraPreview() throws CameraAccessException {
+        SurfaceTexture texture = textureView.getSurfaceTexture();
+        texture.setDefaultBufferSize(imageDimensions.getWidth(),imageDimensions.getHeight());
+
+        Surface surface = new Surface(texture);
+
+        captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+        captureRequest.addTarget(surface);
+
+        cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession session) {
+                if(cameraDevice == null)
+                {
+                    return;
+                }
+
+                cameraSession = session;
+
+                try {
+                    updatePreview();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+            }
+        },null);
+
+    }
+
+    private void updatePreview() throws CameraAccessException {
+        if(cameraDevice == null)
+        {
+            return;
+        }
+
+        captureRequest.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        cameraSession.setRepeatingRequest(captureRequest.build(),null,backgroundHandler);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startBackgroundThread();
+
+        if(textureView.isAvailable())
+        {
+            try {
+                openCamera();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    private void startBackgroundThread() {
+        handlerThread = new HandlerThread("Camera Background");
+        handlerThread.start();
+
+        backgroundHandler = new Handler(handlerThread.getLooper());
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        try {
+            stopBackgroundThread();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        super.onPause();
+    }
+
+    private void stopBackgroundThread() throws InterruptedException {
+        handlerThread.quitSafely();
+        handlerThread.join();
+
+        backgroundHandler = null;
+        handlerThread = null;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
