aab12eae0943b666faef56b7d6b54df460e566e7
cheythomas <tcheyenne360@yahoo.com>
2018-11-14 22:14:44 -0800

    Camera implemented

--- app/src/main/java/zodaproductions/armathtutor/Tab2Fragment.java
+++ app/src/main/java/zodaproductions/armathtutor/Tab2Fragment.java
@@ -1,19 +1,46 @@
 package zodaproductions.armathtutor;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.hardware.Camera;
+import com.camerakit.CameraKitView;
 
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
+
+    private CameraKitView cameraKitView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_fragment,container,false);
+        cameraKitView = view.findViewById(R.id.camera);
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+       cameraKitView.onPause();
+       super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+
+
 }
