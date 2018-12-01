aa20b836c47f754fe617baf2a8145b424f56b558
abutanda <abutanda@csub.ed>
2018-12-01 13:32:45 -0800

    firstcommit

--- app/src/main/java/zodaproductions/armathtutor/Tab1Fragment.java
+++ app/src/main/java/zodaproductions/armathtutor/Tab1Fragment.java
@@ -12,9 +12,82 @@ public class Tab1Fragment extends Fragment {
     private static final String TAG = "Tab1Fragment";
 
     @Nullable
-    @Override
+    //@Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+        View view = inflater.inflate(R.layout.tab1_fragment, container, false);
         return view;
     }
+
+
 }
+    public class WolframAlpha extends Activity {
+
+        private static String AppId = "LG7KVL-QJV2T43KWX";
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            new YourTask().execute();
+        }
+
+
+        WAQueryResult queryResult;
+
+        private class WlframAlpha extends AsyncTask<WAQueryResult, Void, WAQueryResult> {
+
+
+            protected void onPreExecute() {
+
+            }
+
+            @Override
+            protected WAQueryResult doInBackground(WAQueryResult... urls) {
+                String input = "who is the president";
+                WAEngine engine = new WAEngine();
+                engine.setAppID(appid);
+                engine.addFormat("plaintext");
+
+                // Create the query.
+                WAQuery query = engine.createQuery();
+                query.setInput(input);
+                queryResult = null;
+                try {
+                    queryResult = engine.performQuery(query);
+                } catch (WAException e) {
+                    e.printStackTrace();
+                }
+                return queryResult;
+            }
+
+            @Override
+            protected void onPostExecute(WAQueryResult response) {
+                if (queryResult.isError()) {
+                    System.out.println("Query error");
+                    System.out.println("  error code: " + queryResult.getErrorCode());
+                    System.out.println("  error message: " + queryResult.getErrorMessage());
+
+                } else if (!queryResult.isSuccess()) {
+                    System.out.println("Query was not understood; no results available.");
+
+                } else {
+
+                    // Got a result.
+                    System.out.println("Successful query. Pods follow:\n");
+                    for (WAPod pod : queryResult.getPods()) {
+                        if (!pod.isError()) {
+                            if (pod.getTitle().equals("Result")) {
+                                System.out.println(pod.getTitle());
+                                for (WASubpod subpod : pod.getSubpods()) {
+                                    for (Object element : subpod.getContents()) {
+                                        if (element instanceof WAPlainText) {
+                                            System.out.println(((WAPlainText) element).getText());
+                                            Toast.makeText(getApplicationContext(),((WAPlainText) element).getText(),Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
