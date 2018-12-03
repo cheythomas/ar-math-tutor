b615da1781426bbbf0e827d3a3ed493d94cdba45
abutanda <abutanda@csub.ed>
2018-12-03 14:17:38 -0800

    update

--- app/src/main/java/zodaproductions/armathtutor/Tab1Fragment.java
+++ app/src/main/java/zodaproductions/armathtutor/Tab1Fragment.java
@@ -1,39 +1,35 @@
 package zodaproductions.armathtutor;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+//import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Tab1Fragment extends Fragment {
-    private static final String TAG = "Tab1Fragment";
-
     @Nullable
-    //@Override
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab1_fragment, container, false);
         return view;
+
     }
+    private static final String TAG = "Tab1Fragment";
+    private static String AppId = "LG7KVL-QJV2T43KWX";
+    public static String Response = " ";
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new YourTask().execute(); //fragment is task ?
 
-
-}
-    public class WolframAlpha extends Activity {
-
-        private static String AppId = "LG7KVL-QJV2T43KWX";
-        public static String Response = " ";
-
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            new YourTask().execute();
-        }
+ //public class  WAQuery extends Fragment{ }
 
 
         WAQueryResult queryResult;
 
-        private class WolframAlpha extends AsyncTask<WAQueryResult, Void, WAQueryResult> {
+        private void WolframAlpha extends AsyncTask<WAQueryResult, Void, WAQueryResult> {
 
 
             protected void onPreExecute() {
@@ -81,7 +77,7 @@ public class Tab1Fragment extends Fragment {
                                     for (Object element : subpod.getContents()) {
                                         if (element instanceof WAPlainText) {
                                             System.out.println(((WAPlainText) element).getText());
-                                            Toast.makeText(getApplicationContext(),((WAPlainText) element).getText(),Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(getApplicationContext(), ((WAPlainText) element).getText(), Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 }
@@ -92,3 +88,5 @@ public class Tab1Fragment extends Fragment {
             }
         }
 
+    }
+}
